55ec3b15341c46d314ed098d18df26513f3cfffc
QemerS <tu201806169@code.edu.az>
2021-10-26 02:54:14 +0400

    Add project files.

---
+++ UchuncuApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UchuncuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //User tərəfindən daxil edilmiş ədədin 2 mislini(2-yə vurmaq) console-da çap edin;
+            Console.WriteLine("Eded daxil edin");
+            int userinEdedi = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Ededin 2 ile hasili = " + (userinEdedi*2));
+        }
+    }
+}
